15f0543bda88b68dee378842cd54a63a4c4f574e
ian-hank <57643516+ian-hank@users.noreply.github.com>
2022-05-24 20:36:42 -0400

    Creating model for acme proifile
    Model was created for the ACME profile. Still need to figure out how the weather API sends the temp on the get call with coordinates.

--- models/profile.js
+++ models/profile.js
@@ -1,1 +1,53 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose
+const { SchemaTypes } = mongoose
 
+//Schema and fields for an ACME Profile
+const profileSchema = new Schema({
+
+    //Provided by front-end
+    personaID: {
+        type: String
+    },
+
+    firstName:{
+        type: String,
+        required: true
+    },
+
+    lastName:{
+        type: String,
+        required: true
+    },
+
+    interests: {
+        tpye: [String],
+        required: true
+    },
+
+    locLatitude: {
+        type: SchemaTypes.Double,
+        required: true
+    },
+
+    locLongitude: {
+        type: SchemaTypes.Double,
+        required: true
+    },
+
+    //Retrieved information from api.weather.gov
+    city: {
+        type: String
+    },
+
+    state: {
+        type: String
+    },
+
+    currentTemp: {
+        //TODO Determine type weather api uses for temperature
+    }
+})
+
+//Exporting Modules (ES6)
+export default mongoose.model('Profile', profileSchema)
